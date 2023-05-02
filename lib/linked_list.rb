f5165571df5a4c30de50be90d119deaf90722dfb
rastko <vucen.rastko@gmail.com>
2023-05-02 12:49:44 +0200

    Complete linked list implementation

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
+
+# LinkedList class, which will represent the full list.
 class LinkedList
   attr_reader :head
+
   def initialize
     @head = nil
   end
@@ -10,10 +13,8 @@ class LinkedList
     return @head = new_node if head.nil?
 
     current_node = head
-    until current_node.next_node.nil?
-      current_node = current_node.next_node
-    end
-    
+    current_node = current_node.next_node until current_node.next_node.nil?
+
     current_node.next_node = new_node
   end
 
@@ -35,9 +36,7 @@ class LinkedList
 
   def tail
     current_node = head
-    until current_node.next_node.nil?
-      current_node = current_node.next_node
-    end
+    current_node = current_node.next_node until current_node.next_node.nil?
 
     current_node
   end
@@ -59,9 +58,7 @@ class LinkedList
     return @head = nil if size.eql?(0) || head.next_node.nil?
 
     current_node = head
-    until current_node.next_node.next_node.nil?
-      current_node = current_node.next_node
-    end
+    current_node = current_node.next_node until current_node.next_node.next_node.nil?
 
     current_node.next_node = nil
   end
@@ -70,6 +67,7 @@ class LinkedList
     current_node = head
     until current_node.nil?
       return true if current_node.value.eql?(value)
+
       current_node = current_node.next_node
     end
 
@@ -81,6 +79,7 @@ class LinkedList
     current_node = head
     until current_node.nil?
       return index if current_node.value.eql?(value)
+
       index += 1
       current_node = current_node.next_node
     end
@@ -88,19 +87,51 @@ class LinkedList
     nil
   end
 
+  def insert_at(value, index)
+    new_node = Node.new(value)
+    if index.eql?(0)
+      new_node.next_node = head.next_node
+      return @head = new_node
+    end
+    counter = 0
+    current_node = head
+    until (counter + 1).eql?(index)
+      counter += 1
+      current_node = current_node.next_node
+    end
+
+    new_node.next_node = current_node.next_node
+    current_node.next_node = new_node
+  end
+
+  def remove_at(index)
+    return @head = head.next_node if index.eql?(0)
+
+    counter = 0
+    current_node = head
+    until (counter + 1).eql?(index)
+      counter += 1
+      current_node = current_node.next_node
+    end
+
+    current_node.next_node = current_node.next_node.next_node
+  end
+
   def to_s
     current_node = head
     until current_node.nil?
       print "( #{current_node.value} ) -> "
-      current_node = current_node.next_node      
+      current_node = current_node.next_node
     end
 
     print "nil\n"
   end
 end
 
+# Node class, containing a #value method and a link to the #next_node, set both as nil by default.
 class Node
   attr_accessor :value, :next_node
+
   def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
@@ -138,3 +169,9 @@ list.to_s
 puts list.contains?(5)
 
 puts list.find(-3)
+
+list.insert_at(7, 6)
+list.to_s
+
+list.remove_at(4)
+list.to_s
