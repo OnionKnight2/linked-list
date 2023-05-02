37c228819aa181e39dde4ab81b2d98e6c2b8f58b
rastko <vucen.rastko@gmail.com>
2023-05-02 11:54:46 +0200

    Add folder for ruby files

---
+++ lib/linked_list.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+class LinkedList
+  attr_accessor :head
+  def initialize
+    @head = nil
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+    return @head = new_node if head.nil?
+
+    current_node = head
+    until current_node.next_node.nil?
+      current_node = current_node.next_node
+    end
+    
+    current_node.next_node = new_node
+  end
+
+  def prepend(value)
+    new_node = Node.new(value, head)
+    @head = new_node
+  end
+
+  def to_s
+    current_node = head
+    until current_node.nil?
+      print "( #{current_node.value} ) -> "
+      current_node = current_node.next_node      
+    end
+
+    print "nil\n"
+  end
+end
+
+class Node
+  attr_accessor :value, :next_node
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+
+  def to_s 
+    value
+  end
+end
+
+list = LinkedList.new
+list.append(5)
+list.append(12)
+list.append(33)
+list.append(0)
+list.prepend(-3)
+list.prepend(66)
+list.to_s
